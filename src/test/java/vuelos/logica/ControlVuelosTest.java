2f8a1994f7ed3b6eca636bec8d55ce52cfc38fde
SirSalazar <david.1701713821@ucaldas.edu.co>
2022-12-11 22:31:24 -0500

    Pruebas unitarias a ControlVuelos:BuscarRutasConEscala

--- src/test/java/vuelos/logica/ControlVuelosTest.java
+++ src/test/java/vuelos/logica/ControlVuelosTest.java
@@ -13,21 +13,27 @@ public class ControlVuelosTest {
     @Test
     public void origenDestinoEncontrados() {
 
-        List<Ruta> rutas = new ArrayList<>();
+        List<Ruta> rutasEsperadas = new ArrayList<>();
 
         Ruta rutaEsperada1 = new Ruta("Pereira", "Cartagena", 1.5, 400000);
-        Ruta rutaEsperada2 = new Ruta("Pereira", "Cartagena", 2, 340000);
+        Ruta rutaEsperada2 = new Ruta("Pereira", "Cartagena", 3.5, 340000);
 
-        rutas.add(rutaEsperada1);
-        rutas.add(rutaEsperada2);
+        rutasEsperadas.add(rutaEsperada1);
+        rutasEsperadas.add(rutaEsperada2);
 
         ControlVuelos control = new ControlVuelos();
+        List<Ruta> rutasEsperadas2 = new ArrayList<>();
+        control.cargarDatosIniciales();
+        rutasEsperadas2 = control.buscarRutasConEscala("Pereira", "Cartagena");
 
-        List<Ruta> rutas2 = new ArrayList<>();
+        assertEquals(rutasEsperadas.get(0).getOrigen(), rutasEsperadas2.get(0).getOrigen());
+        assertEquals(rutasEsperadas.get(0).getDestino(), rutasEsperadas2.get(0).getDestino());
+        assertEquals(rutasEsperadas.get(0).getDuracion(), rutasEsperadas2.get(0).getDuracion(), 0);
+        assertEquals(rutasEsperadas.get(0).getPrecio(), rutasEsperadas2.get(0).getPrecio(), 0);
 
-        rutas2 = control.buscarRutasConEscala("Pereira", "Cartagena");
-
-        assertEquals(rutaEsperada1, rutas2.get(0));
-
+        assertEquals(rutasEsperadas.get(1).getOrigen(), rutasEsperadas2.get(1).getOrigen());
+        assertEquals(rutasEsperadas.get(1).getDestino(), rutasEsperadas2.get(1).getDestino());
+        assertEquals(rutasEsperadas.get(1).getDuracion(), rutasEsperadas2.get(1).getDuracion(), 0);
+        assertEquals(rutasEsperadas.get(1).getPrecio(), rutasEsperadas2.get(1).getPrecio(), 0);
     }
 }
