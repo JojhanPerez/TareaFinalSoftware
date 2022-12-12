5e34ccbaece57d1ce1297bd200207ea6af8cdbe3
JojhanPerez <jojhan.1702011305@ucaldas.edu.co>
2022-12-11 22:01:53 -0500

    Primear Prueba

--- src/test/java/vuelos/logica/ControlVuelosTest.java
+++ src/test/java/vuelos/logica/ControlVuelosTest.java
@@ -1,10 +1,33 @@
 package vuelos.logica;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import vuelos.entidades.Ruta;
 
 public class ControlVuelosTest {
     @Test
-    public void datosCorrectos() {
-        
+    public void origenDestinoEncontrados() {
+
+        List<Ruta> rutas = new ArrayList<>();
+
+        Ruta rutaEsperada1 = new Ruta("Pereira", "Cartagena", 1.5, 400000);
+        Ruta rutaEsperada2 = new Ruta("Pereira", "Cartagena", 2, 340000);
+
+        rutas.add(rutaEsperada1);
+        rutas.add(rutaEsperada2);
+
+        ControlVuelos control = new ControlVuelos();
+
+        List<Ruta> rutas2 = new ArrayList<>();
+
+        rutas2 = control.buscarRutasConEscala("Pereira", "Cartagena");
+
+        assertEquals(rutaEsperada1, rutas2.get(0));
+
     }
 }
